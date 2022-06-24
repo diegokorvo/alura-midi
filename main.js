aa8845edf0e67bd10600ec0465c8b44928e366aa
Diego Uczak <diegouczak@gmail.com>
2022-06-23 22:57:45 -0300

    enviando projeto aluramidi

---
+++ main.js
@@ -0,0 +1,36 @@
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+
+  if (elemento && elemento.localName === 'audio') {
+    elemento.play();
+  } else {
+    console.log('Elemeto não encontrado ou seletor inválido')
+  }
+}
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+// while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+
+  // console.log(instrumento);
+  const idAudio = `#som_${instrumento}`; //template string
+  // console.log(idAudio);
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  }
+
+  tecla.onkeydown = function (evento) {
+    // console.log(evento.code);
+    if (evento.code === 'Space' || evento.code === 'Enter') {
+      tecla.classList.add('ativa');
+    }
+  }
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
+  }
+
+}
